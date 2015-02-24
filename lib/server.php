46512dd7d188f209496e2950660285571d5337ce
flawwan <flawwan@gmail.com>
2015-02-24 09:43:12 +0100

    fix

--- lib/server.php
+++ lib/server.php
@@ -44,12 +44,15 @@ class Server
 			//Nu vet vi att servern har skickat förfrågan samt att vi nu måste lägga till spelarna i vår databas.
 			$this->db->beginTransaction();
 
-			//Börjar med att skapa en match
-			$sth = $this->db->query("INSERT INTO `matches`() VALUES()");
-			$matchID = $this->db->lastInsertId();
 
 			//Lägg sedan till alla spelare i players vektorn med den matchens id som returnerades ovan.
 			$players = isset($_POST['keys']) ? json_decode($_POST['keys']) : array();
+
+			//Börjar med att skapa en match
+			$sth = $this->db->prepare("INSERT INTO `matches`(`turn`) VALUES(0)");
+			$sth->execute();
+			$matchID = $this->db->lastInsertId();
+
 			$this->createPlayers($players, $matchID);
 		} else {
 			header("HTTP/1.1 403 Unauthorized");
@@ -59,12 +62,23 @@ class Server
 
 	private function createPlayers($players, $matchID)
 	{
+		$playerIndex = 0;
 		$sth = $this->db->prepare("INSERT INTO `players`(`player`,`match_id`,`name`) VALUES(:player,:matchID, :name)");
 		foreach ($players as $player) {
 			$sth->bindParam(':player', $player[0]);
 			$sth->bindParam(':name', $player[1]);
 			$sth->bindParam(':matchID', $matchID);
 			$sth->execute();
+
+			$playerID = $this->db->lastInsertId();
+			if ($playerIndex == 0) {
+				//Börjar med att skapa en match
+				$sthM = $this->db->prepare("UPDATE `matches` SET `turn`=:turn WHERE `match_id`=:match_id");
+				$sthM->bindParam(":turn", $playerID);
+				$sthM->bindParam(":match_id", $matchID);
+				$sthM->execute();
+			}
+			$playerIndex++;
 		}
 		$this->db->commit();
 	}
